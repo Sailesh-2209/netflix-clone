ae6ba17d2f256c8d07f61bf1417e7aedea0400b2
Sailesh-2209 <saileshr2209@gmail.com>
2021-03-26 19:46:42 +0530

    added auth protection to routes

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import * as ROUTES from "./constants/routes";
 import { Home, Signup, Browse, Signin } from "./pages";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+  const user = false;
   return (
     <Router>
-      <Route exact path={ROUTES.SIGN_UP}>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
         <Signup />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
         <Signin />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.BROWSE}>
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
         <Browse />
-      </Route>
+      </ProtectedRoute>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact
+      >
+        <Home />
+      </IsUserRedirect>
     </Router>
   );
 }
